b23a90b0f3db93a4df364eda481774bb9c08e64c
jptx1234 <jptx1234@163.com>
2021-01-17 22:21:06 +0800

    直接依赖MP中的Model，不再搬运代码，便于测试修复效果

--- src/main/java/me/jptx/mp/model/test/entity/H2StudentWithLog.java
+++ src/main/java/me/jptx/mp/model/test/entity/H2StudentWithLog.java
@@ -1,21 +1,21 @@
 package me.jptx.mp.model.test.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.extension.activerecord.ModelWithLog;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 /**
- * 学生实体（带有log对象）
+ * 学生实体（继承MP中的Model，带有log对象）
  */
 @Data
 @Accessors(chain = true)
 @TableName("h2student")
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class H2StudentWithLog extends ModelWithLog<H2StudentWithLog> {
+public class H2StudentWithLog extends Model<H2StudentWithLog> {
 
 
   /**
